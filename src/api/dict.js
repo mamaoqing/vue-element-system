aab042e5747305adadd769690d02045cc4538518
geqx <1255940707@qq.com>
2020-08-10 12:39:05 +0800

    字典js

---
+++ src/api/dict.js
@@ -0,0 +1,41 @@
+import request from '../utils/request';
+
+export const addDict = query => {
+
+    return request({
+        url: '/api/sDict/insertDict',
+        method: 'post',
+        data: query
+    });
+};
+
+export const deleteDict = id => {
+
+    return request({
+        url: '/api/sDict/'+id,
+        method: 'delete'
+    });
+};
+
+export const updateDict = query => {
+
+    return request({
+        url: '/api/sDict/updateDict',
+        method: 'put',
+        data: query
+    });
+};
+export const listDict = query => {
+    return request({
+        url: '/api/sDict/listDict',
+        method: 'get',
+        params: query
+    });
+};
+export const listDictAll = query => {
+    return request({
+        url: '/api/sDict/listDictAll',
+        method: 'get',
+        params: query
+    });
+};
