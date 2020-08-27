5d1bf3b9665bea916e3a83b998399024e7076b6d
mzhongcai <javamzc@126.com>
2020-08-27 09:14:30 +0800

    仪表的js

---
+++ src/api/meter.js
@@ -0,0 +1,55 @@
+import request from '../utils/request';
+
+export const insertMeter = query => {
+    return request({
+        url: '/api/fMeter/insertMeter',
+        method: 'post',
+        data: query
+    });
+};
+export const exportXlsByT = query => {
+    return request({
+        url: '/api/fMeter/export',
+        method: 'post',
+        params: query,
+        responseType: 'blob'
+    });
+};
+export const deleteMeter = id => {
+
+    return request({
+        url: '/api/fMeter/'+id,
+        method: 'delete'
+    });
+};
+export const upload = file => {
+
+    return request({
+        url: '/api/fMeter/upload',
+        method: 'post',
+        data: file
+    });
+};
+export const updateMeter = query => {
+
+    return request({
+        url: '/api/fMeter/updateMeter',
+        method: 'put',
+        data: query
+    });
+};
+export const listMeter = query => {
+    return request({
+        url: '/api/fMeter/listMeter',
+        method: 'get',
+        params: query
+    });
+};
+export const listMeterNum = query => {
+    return request({
+        url: '/api/fMeter/listMeterNum',
+        method: 'get',
+        params: query
+    });
+};
+
