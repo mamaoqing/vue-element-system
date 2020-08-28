a2ec733a53160f31233cffd5cb48b9eb583d1b77
mzhongcai <javamzc@126.com>
2020-08-28 18:50:48 +0800

    仪表抄表js

---
+++ src/api/meterRecord.js
@@ -0,0 +1,55 @@
+import request from '../utils/request';
+
+export const insertMeterRecord = query => {
+    return request({
+        url: '/api/fMeterRecord/insertMeterRecord',
+        method: 'post',
+        data: query
+    });
+};
+export const exportXlsByT = query => {
+    return request({
+        url: '/api/fMeterRecord/export',
+        method: 'post',
+        params: query,
+        responseType: 'blob'
+    });
+};
+export const deleteMeterRecord = id => {
+
+    return request({
+        url: '/api/fMeterRecord/'+id,
+        method: 'delete'
+    });
+};
+export const upload = file => {
+
+    return request({
+        url: '/api/fMeterRecord/upload',
+        method: 'post',
+        data: file
+    });
+};
+export const updateMeterRecord = query => {
+
+    return request({
+        url: '/api/fMeterRecord/updateMeterRecord',
+        method: 'put',
+        data: query
+    });
+};
+export const listMeterRecord = query => {
+    return request({
+        url: '/api/fMeterRecord/listMeterRecord',
+        method: 'get',
+        params: query
+    });
+};
+export const listMeterRecordNum = query => {
+    return request({
+        url: '/api/fMeterRecord/listMeterRecordNum',
+        method: 'get',
+        params: query
+    });
+};
+
