e25ec5c58d06d9aa37dda0745aeeaebb6adc51f6
mzhongcai <javamzc@126.com>
2020-09-14 17:42:31 +0800

    账号js

---
+++ src/api/tariff/account.js
@@ -0,0 +1,55 @@
+import request from '../../utils/request';
+
+export const listAccount = query => {
+    return request({
+        url: '/order/fAccount/listAccount',
+        method: 'get',
+        params: query
+    });
+};
+export const insertAccount = query => {
+    return request({
+        url: '/order/fAccount/insertAccount',
+        method: 'post',
+        data: query
+    });
+};
+export const updateAccount = query => {
+
+    return request({
+        url: '/order/fAccount/updateAccount',
+        method: 'put',
+        data: query
+    });
+};
+export const getRuleList = ownerId => {
+    return request({
+        url: 'order/fCostRule/getRuleByOwnerId/'+ownerId,
+        method: 'get'
+    });
+};
+export const getAccount = query => {
+
+    return request({
+        url: '/order/fAccount/getAccount',
+        method: 'post',
+        params: query
+    });
+};
+export const listFinanceRecord = query => {
+
+    return request({
+        url: '/order/fFinanceRecord/listFinanceRecord',
+        method: 'get',
+        params: query
+    });
+};
+
+export const listFinanceBillRecord = query => {
+
+    return request({
+        url: '/order/fFinanceBillRecord/listFinanceBillRecord',
+        method: 'get',
+        params: query
+    });
+};
