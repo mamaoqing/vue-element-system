c79d8e8cdc33c369674b9a053004a15a02488ea9
chenfangyuan <2237304258@qq.com>
2020-09-15 17:01:35 +0800

    缴费修改

--- src/api/tariff/bill.js
+++ src/api/tariff/bill.js
@@ -138,6 +138,14 @@ export const listBills = query => {
         method: 'get',
     });
 };
+
+export const getAccountByOwnerId = ownerId => {
+    return request({
+        url: '/order/fAccount/getAccountByOwnerId/'+ownerId,
+        method: 'get',
+    });
+};
+
 //BillRecord
 export const billRecord = query => {
     return request({
